33d3318d7dcb4594a66f0b6e7438ee92340a48ad
Kok Chuan Yong <chuan.yong.kok@accenture.com>
2024-04-19 18:05:26 +0800

    fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from 'next/link'
 
 function index() {
-  const { user, error, isLoading } = useUser();
 
-  return <a href='/api/auth/login'><b>Login</b></a>;
+  return <Link href='/api/auth/login'><b>Login</b></Link>;
 }
 
 export default index;
